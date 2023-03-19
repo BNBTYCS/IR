e7b6ec5a7e0ed5c4c78382990fc452f346ad389e
BNBTYCS <127993769+BNBTYCS@users.noreply.github.com>
2023-03-19 16:01:32 +0530

    Update PROGRAM TO PARSE XML.py

--- PROGRAM TO PARSE XML.py
+++ PROGRAM TO PARSE XML.py
@@ -1,4 +1,3 @@
-
 import csv
 import requests
 import xml.etree.ElementTree as ET
@@ -8,31 +7,29 @@ def loadRSS():
     resp = requests.get(url)
     with open('topnewsfeed.xml', 'wb') as f:
         f.write(resp.content)
-
-
 def parseXML(xmlfile):
     tree = ET.parse(xmlfile)
     root = tree.getroot()
-    newsitems=[]
+    newsitems = []
     for item in root.findall('./channel/item'):
         news = {}
         for child in item:
-            if child.tag =='{http://search.yahoo.com/mrss/}content':
-                news['media']=child.attrib['url']
-        else:
-            news[child.tag]=child.text.encode('utf8')
-            newsitems.append(news)
+            if child.tag == '{http://search.yahoo.com/mrss/}content':
+                news['media'] = child.attrib['url']
+            else:
+                news[child.tag] = child.text.encode('utf8')
+        newsitems.append(news)
     return newsitems
-
 def savetoCSV(newsitems, filename):
-    fields = ['guid', 'title', 'pubDate', 'description', 'link','media']
+    fields = ['guid', 'title', 'pubDate', 'description', 'link', 'media']
     with open(filename, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
         writer.writerows(newsitems)
-    loadRSS()
-    newsitems = parseXML('topnewsfeed.xml')
-    savetoCSV(newsitems, 'topnews.csv')
+
+loadRSS()
+newsitems = parseXML('topnewsfeed.xml')
+savetoCSV(newsitems, 'topnews.csv')
 
 def generate_edges(graph):
     edges=[]
@@ -41,3 +38,11 @@ def generate_edges(graph):
             edges.append((node,neighbour))
         return edges
 
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F']
+}
+
+edges = generate_edges(graph)
+print(edges)
